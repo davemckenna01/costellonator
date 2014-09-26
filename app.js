06385c85a46551d68ad0609427be5ba9732e6e13
Dave McKenna <davemckenna01@gmail.com>
2014-09-26 14:52:17 -0400

    change port

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var app = express();
 
+var PORT = 3001;
+
 app.engine('jade', require('jade').__express);
 app.use(express.static(__dirname + '/_sockjs'));
 
@@ -8,6 +10,6 @@ app.get('/', function (req, res) {
   res.render('index.jade', { title: 'Hey', message: 'Hello there!'});
 })
 
-var server = app.listen(3000, function() {
+var server = app.listen(PORT, function() {
     console.log('Listening on port %d', server.address().port);
 });
