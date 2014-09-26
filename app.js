9d0d40786fd5e88736a06454e83c3f26ee7c068b
Dave McKenna <davemckenna01@gmail.com>
2014-09-26 10:17:38 -0400

    initial commit

---
+++ app.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var app = express();
+
+app.engine('jade', require('jade').__express);
+
+app.get('/', function (req, res) {
+  res.render('index.jade', { title: 'Hey', message: 'Hello there!'});
+})
+
+var server = app.listen(3000, function() {
+    console.log('Listening on port %d', server.address().port);
+});
