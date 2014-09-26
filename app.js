93a3d3fef96e92bfeb57d1eb6954d5ba79d2be37
Dave McKenna <davemckenna01@gmail.com>
2014-09-26 14:54:05 -0400

    add logging

--- app.js
+++ app.js
@@ -7,7 +7,8 @@ app.engine('jade', require('jade').__express);
 app.use(express.static(__dirname + '/_sockjs'));
 
 app.get('/', function (req, res) {
-  res.render('index.jade', { title: 'Hey', message: 'Hello there!'});
+    console.log('got request'); 
+    res.render('index.jade', { title: 'Hey', message: 'Hello there!'});
 })
 
 var server = app.listen(PORT, function() {
